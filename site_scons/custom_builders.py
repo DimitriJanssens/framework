59a632ee4c45da93b88983f6217f2e55297e76ef
Dimitri Janssens <dimitri.janssens@sioux.eu>
2016-09-14 10:17:36 +0200

    Made correction in dirs for cpppath in custom_builders. Renamed export to globalincludes

--- site_scons/custom_builders.py
+++ site_scons/custom_builders.py
@@ -1,30 +1,30 @@
 def App(env, *nargs, **kwargs):
-	cpenv = env.Clone()
-	cpenv['CPPPATH'].append(cpenv.Dir('../header').srcnode().abspath)
+	env = env.Clone()
+	env['CPPPATH'].append(env.Dir('../header').srcnode())
 
-	cpenv.Prepend(_LIBFLAGS="-Wl,--start-group ")  # the trailing space is required
-	cpenv.Append(_LIBFLAGS=" -Wl,--end-group")  # the trailing space is required
+	env.Prepend(_LIBFLAGS="-Wl,--start-group ")  # the trailing space is required
+	env.Append(_LIBFLAGS=" -Wl,--end-group")  # the trailing space is required
 
-	if cpenv.GetOption('unittests') == True:
-		cpenv['CPPPATH'].append([ '%s/check' %(cpenv['INC_DIR']) ])
+	if env.GetOption('unittests') == True:
+		env['CPPPATH'].append([ env.Dir('%s/check' %(env['INC_DIR'])) ])
 		kwargs.get('LIBS').append([ 'testframework', 'check', 'gcov' ])
 
 	for f in kwargs.get('LIBS'):
 		if 	'osthreading' in f:
 			kwargs.get('LIBS').append(['pthread'])	
 
-	_bin = cpenv.Program(*nargs, LIBPATH = cpenv['LIB_DIR'], **kwargs)
-	_bin = cpenv.Install(cpenv['BIN_DIR'], _bin)
+	_bin = env.Program(*nargs, LIBPATH = env['LIB_DIR'], **kwargs)
+	_bin = env.Install(env['BIN_DIR'], _bin)
 	return _bin
 
 def Lib(env, *nargs, **kwargs):
-	cpenv = env.Clone()
-	cpenv['CPPPATH'].append(cpenv.Dir('../header').srcnode().abspath)
+	env = env.Clone()
+	env['CPPPATH'].append(env.Dir('../header').srcnode())
 
-	if cpenv.GetOption('unittests') == True:
-		cpenv['CPPPATH'].append(cpenv.Dir('../../header').srcnode().abspath)
-		cpenv['CPPPATH'].append(['%s/check' %(cpenv['INC_DIR'])])
+	if env.GetOption('unittests') == True:
+		env['CPPPATH'].append(env.Dir('../../header').srcnode())
+		env['CPPPATH'].append([ env.Dir('%s/check' %(env['INC_DIR'])) ])
 
-	_lib = cpenv.StaticLibrary(*nargs, **kwargs)
-	_lib = cpenv.Install(cpenv['LIB_DIR'], _lib)
+	_lib = env.StaticLibrary(*nargs, **kwargs)
+	_lib = env.Install(env['LIB_DIR'], _lib)
 	return _lib
